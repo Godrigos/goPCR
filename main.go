00f1edc642a9a9d50b2649485525aeb9c44b63a2
Rodrigo Aluizio <r.aluizio@gmail.com>
2020-09-12 09:10:21 -0300

    reomved fixed window size

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
-import "fyne.io/fyne"
-
 func main() {
 	c := &pcr{}
 	c.w.SetContent(c.loadUI(c.application))
 	c.prefs()
-	c.w.Resize(fyne.NewSize(375, 1))
-	c.w.SetFixedSize(true)
 	c.w.ShowAndRun()
 }
